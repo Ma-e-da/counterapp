f9a3a8b8b2c61bfdbad53dd912d238f18f4bc3cd
Yoshie Maeda <cassisism@gmail.com>
2024-06-25 20:56:19 +0900

    React SDK (thirdweb/react)よりThirdwebProviderをセットした

--- utils/constants.ts
+++ utils/constants.ts
@@ -5,3 +5,7 @@ const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID;
 export const client = createThirdwebClient({
   clientId: CLIENT_ID as string,
 });
+
+import { defineChain } from "thirdweb";
+
+export const chain = defineChain(11155111);
