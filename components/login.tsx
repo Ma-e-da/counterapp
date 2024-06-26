af41d5d01cd49b11f86c7b850b470f68d41c7e26
Yoshie Maeda <cassisism@gmail.com>
2024-06-26 21:36:41 +0900

    ログインコンポーネントを作って設置

--- components/login.tsx
+++ components/login.tsx
@@ -1,10 +1,42 @@
-import { ConnectButton } from "thirdweb/react";
+"use client";
+
+import { ConnectButton, useActiveAccount } from "thirdweb/react";
 import { chain, client } from "../utils/constants";
+import Counter from "./counter";
 
 const Login: React.FC = () => {
+  const account = useActiveAccount();
+
   return (
-    <div>
-      <ConnectButton client={client} chain={chain} />
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      {account ? (
+        <div style={{ textAlign: "center" }}>
+          <ConnectButton
+            client={client}
+            chain={chain}
+            connectModal={{
+              size: "compact",
+            }}
+          />
+          <Counter />
+        </div>
+      ) : (
+        <ConnectButton
+          client={client}
+          chain={chain}
+          connectModal={{
+            size: "compact",
+          }}
+        />
+      )}
     </div>
   );
 };
